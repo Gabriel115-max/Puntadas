e322b81f2c9ea09ea55d17f113149dcbcb0ea6db
Gabriel115-max <83608152+Gabriel115-max@users.noreply.github.com>
2022-03-07 20:56:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener("click", () => {
+    document.querySelector('.nav-menu').classList .tooggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.Product',{delay: 500});
+ScrollReveal().reveal('.cards-banner-one',{delay: 500});
+ScrollReveal().reveal('.cards-banner-two',{delay: 500});
+    
